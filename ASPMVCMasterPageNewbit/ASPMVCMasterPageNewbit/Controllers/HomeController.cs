ef793be25ee9d4cc9aba612e6749159050bb1c54
khuongsatou <yoshi240499@gmail.com>
2019-10-26 02:47:55 +0700

    newbit demo

---
+++ ASPMVCMasterPageNewbit/ASPMVCMasterPageNewbit/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPMVCMasterPageNewbit.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //ok
+            return RedirectToAction("../TrangChu");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
